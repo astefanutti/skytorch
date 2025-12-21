f0c6d2351739a92aae05e18d144bde52b09beabb
Antonin Stefanutti <antonin@stefanutti.fr>
2025-12-21 14:53:10 +0100

    Rename service client

--- kpu/torch/client/__init__.py
+++ kpu/torch/client/__init__.py
@@ -2,7 +2,7 @@
 KPU PyTorch gRPC client for tensor streaming.
 """
 
-from kpu.torch.client.example_client import TensorClient
+from kpu.torch.client.service import TensorClient
 
 __all__ = [
     "TensorClient",
