0e324d3c53520aaa434e908fee3a547bed29d022
Antonin Stefanutti <antonin@stefanutti.fr>
2026-01-06 19:23:45 +0100

    Add compute decorator

--- kpu/client/__init__.py
+++ kpu/client/__init__.py
@@ -4,11 +4,13 @@ from kpu.client.compute import Compute
 from kpu.client.cluster import Cluster
 from kpu.client.event import log_event
 from kpu.client.grpc import GRPCClient
+from kpu.client.decorator import compute
 
 __all__ = [
     "Compute",
     "Cluster",
     "GRPCClient",
+    "compute",
     "init",
     "default_namespace",
     "log_event",
